da2e82b761f21e07b18d5c73c1300c7d1521d9b9
Stephen Jones <stephen-jones@hotmail.co.uk>
2022-03-11 14:23:14 +0000

    Timeline error fixed

--- src/features/projects/Timeline.js
+++ src/features/projects/Timeline.js
@@ -5,15 +5,52 @@ import { ProjectTimeline } from './components/ProjectTimeline';
 export function Timeline() {
     const selectProjects = (state) => state.projects;
     const projects = useSelector(selectProjects);
+    let array = projects.map((item) => (
+        {...item, timeline: 0}),
+
+    );
+    let arr = [];
+
+    array.forEach((item, index) => {
+        const createdAt = new Date(item.createdAt);
+        const dueDate = new Date(item.dueDate);
+    
+        const start = createdAt;
+        const end = dueDate;
+        const today = new Date();
+    
+        const q = Math.abs(today-start);
+        const d = Math.abs(end-start);
+
+        if(start > today) {
+            let rounded = 0;
+            arr.push(rounded);
+
+        } else if (today > end) {
+            let rounded = 100;
+            arr.push(rounded);
+    
+        } else {
+            let rounded = Math.round((q/d)*100);
+            arr.push(rounded);
+
+            
+        }
+        console.log(arr);
+        item.timeline = arr[index];
+
+    });
 
 
-    console.log(projects);
+ 
+    console.log(array);
+
     return (  
         <div className="">
             <div>Timeline</div>
 
-            {projects.map((project) => (
-                < ProjectTimeline key={project.projectId} id={project.projectId} projectList={projects} />
+            {array.map((project) => (
+                < ProjectTimeline key={project.projectId} id={project.projectId} projectList={projects} timelineSort={project.timeline} />
             ))}
 
         </div>
--- src/features/projects/components/ProjectTimeline.js
+++ src/features/projects/components/ProjectTimeline.js
@@ -8,41 +8,12 @@ import moment from 'moment';
 
 <FontAwesomeIcon icon="fa-solid fa-note-sticky" />
 
-export function ProjectTimeline({ id, projectList }) {
+export function ProjectTimeline({ id, projectList, timelineSort }) {
     const [startDate, setStartDate] = useState(new Date());
     const dispatch = useDispatch();
 
     const project = () => {
         return projectList.find((project) => project.projectId === id);
-    };
-    const createdAt = new Date(project().createdAt);
-    const dueDate = new Date(project().dueDate);
-
-    const start = createdAt;
-    const end = dueDate;
-    const today = new Date();
-
-    const q = Math.abs(today-start);
-    const d = Math.abs(end-start);
-    const rounded = Math.round((q/d)*100);
-
-    console.log(createdAt);
-    
-    const reenderPercentage = () => {
-        if(start > today) {
-            const percentage = 100;
-            return <ProgressBar now={percentage} label={`0%`} />
-
-        } if (today > end) {
-            const percentage = 100;
-            return <ProgressBar now={percentage} label={`Due`} />
-
-        } else {
-            const percentage = rounded;
-            return <ProgressBar now={percentage} label={`${percentage}%`} />
-        
-        }
-
     };
 
     const updateCreatedAt = (date) => {
@@ -61,11 +32,13 @@ export function ProjectTimeline({ id, projectList }) {
 
     }
 
+    // timelineSort(timelinePercentage());
+
     return (  
         <li className="d-flex my-3 shadow-sm rounded bg-light" >
             <div>{project().projectName}</div>
             <div style={{width: "100%"}}>
-                {reenderPercentage()}
+                <ProgressBar now={timelineSort} label={timelineSort === 100 ? 'Due' : `${timelineSort}%`} />
                 <Calendar databaseDate={project().createdAt} updateDate={updateCreatedAt}/>
                 <Calendar databaseDate={project().dueDate} updateDate={updateDue}/>
             </div>
