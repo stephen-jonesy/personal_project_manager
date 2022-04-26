0447d6801e980ffa049f6e470f4d2235aad79e11
Stephen Jones <stephen-jones@hotmail.co.uk>
2022-04-26 16:35:44 +0100

    userSlice removed

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import projectsReducer from '../features/projects/projectsSlice';
 
+
 export const store = configureStore({
   reducer: {
     projects: projectsReducer
